3c60e605665818ed443a9bdc1e9743761215f16c
J.T <gisfromscratch@live.de>
2019-05-09 19:07:23 +0200

    #2 Using a spatial filter on a GeoJSON file

--- src/gdal-testing/gdal-testing/test.cpp
+++ src/gdal-testing/gdal-testing/test.cpp
@@ -43,6 +43,51 @@ TEST_F(GdalTestSuite, ReadGeoJson) {
 	}
 }
 
+
+TEST_F(GdalTestSuite, FilterGeoJson) {
+	OGRLinearRing ring;
+	ring.addPoint(7.128, 50.721);
+	ring.addPoint(7.128, 50.710);
+	ring.addPoint(7.147, 50.710);
+	ring.addPoint(7.147, 50.721);
+	ring.addPoint(7.128, 50.721);
+	OGRSpatialReference wgs84;
+	wgs84.SetWellKnownGeogCS("WGS84");
+	ring.assignSpatialReference(&wgs84);
+	OGRPolygon area;
+	area.assignSpatialReference(&wgs84);
+	area.addRing(&ring);
+
+	auto filepath = GdalTestSuite::to_path("geojson.json");
+	auto dataset = GdalManagedDataset::open_read(filepath);
+	EXPECT_TRUE(dataset->is_valid()) << "Dataset is valid.";
+
+	auto layer_names = dataset->get_layer_names();
+	EXPECT_FALSE(layer_names.empty()) << "Layer names must not be empty.";
+	for (auto &&layer_name : layer_names)
+	{
+		auto layer = dataset->get_layer(layer_name);
+		EXPECT_NE(nullptr, layer) << "Layer must not be nullptr.";
+
+		layer->SetSpatialFilter(&area);
+		std::list<GIntBig> ids;
+		std::list<int> strecken_ids;
+		for (auto &&feature : layer)
+		{
+			auto geometry = feature->GetGeometryRef();
+			EXPECT_FALSE(geometry->IsEmpty()) << "Geometry must not be empty.";
+			auto fid = feature->GetFID();
+			ids.push_back(fid);
+			auto strecke_id = feature->GetFieldAsInteger("strecke_id");
+			strecken_ids.push_back(strecke_id);
+		}
+		EXPECT_FALSE(ids.empty()) << "At least one feature must be found.";
+		EXPECT_FALSE(strecken_ids.end() == std::find(strecken_ids.begin(), strecken_ids.end(), 47)) << "Strecke 47 must be found.";
+		EXPECT_FALSE(strecken_ids.end() == std::find(strecken_ids.begin(), strecken_ids.end(), 48)) << "Strecke 48 must be found.";
+	}
+}
+
+
 int main(int argc, wchar_t* argv[])
 {
 	using namespace testing;
